e54ab2e181e5280adac6a2ac8fc1d0bbce436197
Anthony Goldbloom <goldbloom@goldbloom-macbookpro3.roam.corp.google.com>
2020-01-21 21:55:08 -0800

    second commit

---
+++ build/precache-manifest.358ded34f85cc4c9f116fa661e9e88fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa4c5e946e0300ca312b48baf0bc292",
+    "url": "/Pairwise/index.html"
+  },
+  {
+    "revision": "b7d8754ad308aa496afb",
+    "url": "/Pairwise/static/css/main.558bfbfb.chunk.css"
+  },
+  {
+    "revision": "fe1211a8c10ff2bf23d5",
+    "url": "/Pairwise/static/js/2.9d4743a5.chunk.js"
+  },
+  {
+    "revision": "6238cae091dda77327be7358b5c18030",
+    "url": "/Pairwise/static/js/2.9d4743a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7d8754ad308aa496afb",
+    "url": "/Pairwise/static/js/main.6b6f6351.chunk.js"
+  },
+  {
+    "revision": "8475f518a984a50baf7d",
+    "url": "/Pairwise/static/js/runtime-main.62d89321.js"
+  }
+]);
