5be417fb37a9c898004c87c71e2d60a2b11df042
Anthony Goldbloom <goldbloom@goldbloom-macbookpro3.roam.corp.google.com>
2020-01-24 08:56:48 -0800

    add label

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ export default class App extends React.Component {
     ReactGA.event({
       category: 'Option Form',
       action: 'Submit Option Form',
-      transport: event.target.ta_options.value
+      opt_label: event.target.ta_options.value
     });
 
 
